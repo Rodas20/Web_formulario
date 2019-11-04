f6a3252c4ccf612f838812585047b1ad73b433e2
Rodas20 <00006417@uca.edu.sv>
2019-11-04 04:30:00 -0600

    Updates

---
+++ precache-manifest.72ce28dc1d045981482bac13d144c3f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67722cd1c03633606dbda6a57a898a7b",
+    "url": "/Web_formulario/index.html"
+  },
+  {
+    "revision": "b953ca4ca0031023b9dc",
+    "url": "/Web_formulario/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b953ca4ca0031023b9dc",
+    "url": "/Web_formulario/static/js/2.7f969312.chunk.js"
+  },
+  {
+    "revision": "f5651c9c27403904ca4b",
+    "url": "/Web_formulario/static/js/main.94cd8d9a.chunk.js"
+  },
+  {
+    "revision": "93195ec7c174f48d43d6",
+    "url": "/Web_formulario/static/js/runtime-main.2baa1c5a.js"
+  }
+]);
